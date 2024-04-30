7d1a9a6c16501ceb65d114bafba421c215a59529
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-04-30 07:42:04 -0300

    feat: add initial search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Header from "@/components/header";
+import Search from "@/components/search";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
+  );
 }
