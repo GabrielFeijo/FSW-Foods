02575b22b1c921b3f3649281e97e59aedabff537
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-04-30 07:34:21 -0300

    feat: add initial header component

---
+++ src/components/header.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between px-5 pt-6">
+      <Image src="/logo.png" alt="FSW Foods" height={30} width={100} />
+
+      <Button
+        size="icon"
+        variant="outline"
+        className="border-none bg-transparent"
+      >
+        <MenuIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default Header;
