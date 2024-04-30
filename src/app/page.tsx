458ee33892e39ac1036f2cb839b3179f70fef333
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2024-04-30 10:42:04 -0300

    feat: add promo banner to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import CategoryList from "@/components/category-list";
 import Header from "@/components/header";
 import Search from "@/components/search";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -12,6 +13,18 @@ export default function Home() {
 
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo_banner1.png"
+          alt="Até 30% de desconto em Pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
--- src/components/category-item.tsx
+++ src/components/category-item.tsx
@@ -8,7 +8,6 @@ interface CategoryItemProps {
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
     <div className="flex items-center gap-3 rounded-full bg-white px-4 py-3 shadow-md">
-      {" "}
       <Image
         src={category.imageUrl}
         alt={category.name}
